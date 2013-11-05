3e4eb5595281933c97685cb36bdcc8cdb864489c
Paul De Schacht <pauldeschacht@gmail.com>
2013-11-05 09:31:01 +0100

    added org mode

--- init.el
+++ init.el
@@ -17,7 +17,8 @@
                       clojure-test-mode
                       nrepl
                       auto-complete
-                      ac-nrepl)
+                      ac-nrepl
+                      org)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -25,3 +26,15 @@
     (package-install p)))
 
 (load "~/.emacs.d/user.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
